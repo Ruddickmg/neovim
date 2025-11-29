6be8a9a60205beef162c8b76a6253c10d0b81dfc
Ruddickmg <ruddickmg@gmail.com>
2025-11-28 15:32:05 -1000

    configure trouble.vim

--- lua/myLuaConf/plugins/trouble.lua
+++ lua/myLuaConf/plugins/trouble.lua
@@ -4,20 +4,14 @@ return {
     for_cat = "general",
     after = function()
       require("trouble").setup({
-        -- your trouble.nvim configuration options
-        -- e.g., use_diagnostic_signs = true,
-        --       fold_open = "",
-        --       fold_closed = "",
-        --       indent_lines = "│",
-        --       signs = {
-        --         error = " ",
-        --         warning = " ",
-        --         info = " ",
-        --         hint = " "
-        --       },
-        --       layout = "bottom",
-        --       size = 10,
+        auto_open = false, -- auto open when there are items
+        auto_preview = true, -- automatically open preview when on an item
+        indent_guides = true, -- show indent guides
+        warn_no_results = false, -- show a warning when there are no results
+        open_no_results = true, -- open the trouble window when there are no results
       })
+      vim.api.nvim_set_hl(0, 'TroubleNormal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'TroubleNormalNC', { bg = 'none' })
     end
   }
 }
