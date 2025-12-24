1db0cbbc57a899a858755c4ef03fa27b9fdb785e
Ruddickmg <ruddickmg@gmail.com>
2025-12-24 08:30:48 -1000

    set up lspmux

--- lua/myLuaConf/LSPs/rust-analyzer.lua
+++ lua/myLuaConf/LSPs/rust-analyzer.lua
@@ -3,16 +3,22 @@ return {
     "rustaceanvim",
     lazy = false,
     after = function()
+      local lspmux_path = "/run/user/1000/lspmux/lspmux.sock"
       vim.g.rustaceanvim = {
         server = {
           on_attach = function() end,
-          default_settings = {
+          cmd = function()
+            return vim.lsp.rpc.connect(lspmux_path)
+          end,
+          settings = {
             ["rust-analyzer"] = {
+              lspMux = {
+                version = "1",
+                method = "connect",
+                server = "rust-analyzer",
+              },
               installCargo = false,
               installRustc = false,
-              cachePriming = {
-                enable = false,
-              },
               check = {
                 command = "clippy", -- use clippy for diagnostics on save
               },
