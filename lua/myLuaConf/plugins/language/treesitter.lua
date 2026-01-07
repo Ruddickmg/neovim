fe450ab4ed0693850ac54e7ed2b4321fca7308de
grant <ruddickmg@gmail.com>
2026-01-06 23:41:56 -1000

    update text objects

--- lua/myLuaConf/plugins/language/treesitter.lua
+++ lua/myLuaConf/plugins/language/treesitter.lua
@@ -125,6 +125,7 @@ return {
       vim.g.no_plugin_maps = true
     end,
     after = function()
+      local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
       require("nvim-treesitter-textobjects").setup({
         select = {
           lookahead = true,
@@ -143,6 +144,17 @@ return {
       for key, target in pairs(keys) do
         set_keymaps(key, target)
       end
+
+      -- Repeat movement with ; and ,
+      -- ensure ; goes forward and , goes backward regardless of the last direction
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
       -- You can also use captures from other query groups like `locals.scm`
       vim.keymap.set({ "x", "o" }, "as", function()
