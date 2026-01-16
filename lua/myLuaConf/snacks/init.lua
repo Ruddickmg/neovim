a821e700d9d09fcf3100d0d6c7f1d6795a0618ec
grant <ruddickmg@gmail.com>
2026-01-16 13:11:41 -1000

    update scroll functionality

--- lua/myLuaConf/snacks/init.lua
+++ lua/myLuaConf/snacks/init.lua
@@ -58,7 +58,25 @@ return {
         layout = { enabled = true },
         quickfile = { enabled = true },
         scope = { enabled = true },
-        scroll = { enabled = true },
+        scroll = {
+          enabled = true,
+          animate = {
+            duration = { step = 10, total = 150 },
+            easing = "linear",
+          },
+          -- faster animation when repeating scroll after delay
+          animate_repeat = {
+            delay = 100, -- delay in ms before using the repeat animation
+            duration = { step = 5, total = 50 },
+            easing = "linear",
+          },
+          -- what buffers to animate
+          filter = function(buf)
+            return vim.g.snacks_scroll ~= false
+              and vim.b[buf].snacks_scroll ~= false
+              and vim.bo[buf].buftype ~= "terminal"
+          end,
+        },
         statuscolumn = { enabled = true },
         words = { enabled = true },
         styles = {
