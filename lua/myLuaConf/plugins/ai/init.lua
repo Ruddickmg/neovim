aa9491421541ad4ecd472fb55cc633dc74e1805b
grant <ruddickmg@gmail.com>
2026-02-16 10:32:13 -1000

    remove agentic nvim

--- lua/myLuaConf/plugins/ai/init.lua
+++ lua/myLuaConf/plugins/ai/init.lua
@@ -1,6 +1,6 @@
 return {
   -- { import = "myLuaConf.plugins.ai.copilot" },
-  { import = "myLuaConf.plugins.ai.agentic" },
+  -- { import = "myLuaConf.plugins.ai.agentic" },
   { import = "myLuaConf.plugins.ai.opencode" },
   { import = "myLuaConf.plugins.ai.sidekick" },
 }
