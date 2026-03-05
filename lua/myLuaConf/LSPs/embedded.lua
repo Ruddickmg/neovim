3cb724ceddad48655d04632254114c21af23e676
grant <ruddickmg@gmail.com>
2026-03-05 08:01:48 -1000

    remove otter log

--- lua/myLuaConf/LSPs/embedded.lua
+++ lua/myLuaConf/LSPs/embedded.lua
@@ -10,7 +10,6 @@ return {
     "otter.nvim",
     ft = { "markdown" },
     after = function()
-      vim.notify("loading otter!")
       require("otter").activate({
         diagnostic_update_events = { "BufWritePost", "InsertLeave" },
         buffers = {
