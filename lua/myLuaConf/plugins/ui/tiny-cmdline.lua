e499eea62f2fdf918a68fd5935f4ae63dd2977a4
grant <ruddickmg@gmail.com>
2026-06-01 20:52:55 -1000

    highlight the search bar differently

--- lua/myLuaConf/plugins/ui/tiny-cmdline.lua
+++ lua/myLuaConf/plugins/ui/tiny-cmdline.lua
@@ -1,38 +1,33 @@
+local hl_group = vim.api.nvim_create_augroup("TinyCmdlineHL", { clear = true })
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  group = hl_group,
+  callback = function()
+    local t = vim.fn.getcmdtype()
+    if t == "/" or t == "?" then
+      vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { fg = "#ff9e64" })
+    else
+      vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { link = "FloatBorder" })
+      vim.api.nvim_set_hl(0, "TinyCmdlineNormal", { link = "MsgArea" })
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  group = hl_group,
+  callback = function()
+    vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { link = "FloatBorder" })
+    vim.api.nvim_set_hl(0, "TinyCmdlineNormal", { link = "MsgArea" })
+  end,
+})
+
 return {
   {
     "tiny-cmdline.nvim",
     after = function()
-      vim.api.nvim_create_autocmd("CmdlineEnter", {
-        group = vim.api.nvim_create_augroup("TinyCmdlineHL", { clear = true }),
-        callback = function()
-          local t = vim.fn.getcmdtype()
-          if t == "/" or t == "?" then
-            vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { fg = "#ff9e64" })
-            vim.api.nvim_set_hl(0, "TinyCmdlineNormal", { bg = "#2d2020" })
-          else
-            vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { link = "FloatBorder" })
-            vim.api.nvim_set_hl(0, "TinyCmdlineNormal", { link = "MsgArea" })
-          end
-        end,
-      })
-
       require("tiny-cmdline").setup({
         on_reposition = require("tiny-cmdline").adapters.blink,
         native_types = {},
-        position = { y = "10%" },
-        title = {
-          enabled = true,
-          pos = "left",
-          formats = {
-            { type = "/", title = "  " },
-            { type = "?", title = "  " },
-            { type = ":", title = "  " },
-            { type = "=", title = "  " },
-            { type = "@", title = " 󰥻 " },
-            { type = ">", title = " 󰅏 " },
-            { title = "  " },
-          },
-        },
+        position = { y = "13%" },
       })
     end,
   },
